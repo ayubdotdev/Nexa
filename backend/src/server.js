290eab27a5947d20c5370356a4c0c49ada60ef29
Ayub Khan <xyzkhn0@gmail.com>
2025-08-29 00:00:35 +0530

    first commit

---
+++ backend/src/server.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import { ENV } from './config/env.js'
+
+const app = express()
+
+app.get("/",(req,res)=>{
+    res.send("Hello World 123!")
+})
+console.log("Mongo uri: ",ENV.MONGO_URI)
+
+app.listen(ENV.PORT,()=>{
+    console.log("Server running on :" ,ENV.PORT)
+})
