ce20bfa2671dafca6254f4c59b0b5a49caaa6702
Ayub Khan <xyzkhn0@gmail.com>
2025-08-29 20:50:46 +0530

    minor bugs fixed

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -26,9 +26,11 @@ const syncUser = inngest.createFunction(
 )
 //delet user
 const deleteUserFromDB = inngest.createFunction(
+
     {id:"delete-user-from-db"},
     {event:"clerk/user.deleted"},
     async({event})=>{
+        await connectDB()
         const {id}= event.data
         await User.deleteOne({clerkId:id})
     }
